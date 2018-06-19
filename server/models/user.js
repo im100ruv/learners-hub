ca15e6375730138ee0ab5d7fa2a0b8b8c2fdd57a
saurabh <saurabh.karna@mountblue.io>
2018-06-19 19:01:25 +0530

    update schema for user and course

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
-    name: String
+    id: String,
+    name: String,
+    email: String,
+    password: String,
+    mobile: String,
+    user_type: String
 });
 
 module.exports = mongoose.model('user', schema);
