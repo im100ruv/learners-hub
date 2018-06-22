2a4058118f93e948c976421d1e30369b2333945b
abhay.gupta <abhay.gupta@mountblue.io>
2018-06-22 20:45:43 +0530

    add uplaod button and also set state in every field

--- client/src/components/createCourse/CreateCourse.js
+++ client/src/components/createCourse/CreateCourse.js
@@ -2,8 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-import Button from '@material-ui/core/Button'
-import './CreateCourse.css'
+import FileUpload from "@material-ui/icons/FileUpload";
+import AddCircle from "@material-ui/icons/AddCircle";
+import Delete from "@material-ui/icons/Delete";
+import Button from "@material-ui/core/Button";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import red from "@material-ui/core/colors/red";
+import "./CreateCourse.css";
+
+const primary = red[500]; // #F44336
 
 const styles = theme => ({
   container: {
@@ -11,8 +18,9 @@ const styles = theme => ({
     flexWrap: "wrap",
     margin: "auto",
     width: "40%",
-    border: '1px ridge',
-    backgroundColor: "#e4f1e566"
+    border: "1px ridge",
+    backgroundColor: "#e4f1e566",
+    marginTop: "1em"
   },
   textField: {
     marginLeft: theme.spacing.unit,
@@ -22,16 +30,15 @@ const styles = theme => ({
   menu: {
     width: "100%"
   },
-  discBorder:{
-        marginLeft: "10px",
-       
+  discBorder: {
+    marginLeft: "10px"
   },
   button: {
-    margin: theme.spacing.unit,
+    margin: theme.spacing.unit
   },
   input: {
-    display: 'none',
-  },
+    display: "none"
+  }
 });
 
 const diffLevel = [
@@ -65,40 +72,61 @@ const categories = [
 
 class CreateCourse extends React.Component {
   state = {
-    name: "Java crash course",
-    multiline: "Controlled"
+    courseName: "",
+    courseCategory:"",
+    courseURL:"",
+    courseDuration:"",
+    diffecultyLevel:"Beginner",
+    summary:"",
+    
   };
-
+  handleChange = stateName => event => {
+    event.preventDefault();
+    this.setState({
+      [stateName]: event.target.value.trim(),
+    });
+    return;
+  };
+  setData=()=>{
+    console.log(this.state);
+  }
   render() {
     const { classes } = this.props;
 
     return (
-        
       <form className={classes.container} noValidate autoComplete="off">
+        <p className="uploadCourse">UPLOAD NEW COURSE</p>
         <TextField
           id="courseName"
           label="Name of Course"
           className={classes.textField}
           margin="normal"
+          onChange={this.handleChange('courseName')}
         />
-          <input
-        accept="image/*"
-        className={classes.input}
-        id="flat-button-file"
-        multiple
-        type="file"
-      />
-        <label htmlFor="flat-button-file">
+        <input
+          accept="image/*"
+          className={classes.input}
+          id="flat-button-file"
+          multiple
+          type="file"
+        />
         <TextField
           id="uploadCourse"
           label="Upload course file"
           className={classes.textField}
           margin="normal"
-        >
-        </TextField>
-        </label>
-       
-      {/* <label htmlFor="flat-button-file">
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <label htmlFor="flat-button-file">
+                  <AddCircle />
+                </label>
+              </InputAdornment>
+            )
+          }}
+        />
+
+        {/* <label htmlFor="flat-button-file">
         <Button component="span" className={classes.button}>
           Upload
         </Button>
@@ -108,28 +136,20 @@ class CreateCourse extends React.Component {
           label="Course URL"
           className={classes.textField}
           margin="normal"
+          onChange={this.handleChange('courseURL')}
         />
 
         <TextField
           id="categories"
-          select
           label="Categories"
-          className={classes.textField }
+          className={classes.textField}
           value={this.state.categories}
-          SelectProps={{
-            native: true,
-            MenuProps: {
-              className: classes.menu
-            }
-          }}
-          helperText="Please select course category"
+          
+          placeholder="Enter comma seperated course category"
           margin="normal"
+          onChange={this.handleChange('courseCategory')}
         >
-          {categories.map(option => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
+          
         </TextField>
         <TextField
           id="number"
@@ -140,6 +160,7 @@ class CreateCourse extends React.Component {
             shrink: true
           }}
           margin="normal"
+          onChange={this.handleChange('courseDuration')}
         />
         <TextField
           id="diffecultyLevel"
@@ -153,6 +174,7 @@ class CreateCourse extends React.Component {
             }
           }}
           margin="normal"
+          onChange={this.handleChange('diffecultyLevel')}
         >
           {diffLevel.map(option => (
             <option key={option.value} value={option.value}>
@@ -161,13 +183,33 @@ class CreateCourse extends React.Component {
           ))}
         </TextField>
         <TextField
-          id="multiline-static"
-          label="Description"
+          id="summary"
+          label="summary of course"
           multiline
           rows="7"
           className={[classes.textField, classes.discBorder]}
           margin="normal"
+          onChange={this.handleChange('summary')}
         />
+        <div className="uploadButton">
+          <Button
+            variant="contained"
+            color="secondary"
+            className={classes.button}
+          >
+            Cancel
+            <Delete />
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            onClick={this.setData}
+          >
+            Upload
+            <FileUpload />
+          </Button>
+        </div>
       </form>
     );
   }
