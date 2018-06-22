4e45723f6b9a2149c675034a8872cb2c0d43c4fc
saurabh <saurabh.karna@mountblue.io>
2018-06-22 19:55:33 +0530

    add course resource materials

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import CourseDetail from './components/courseDetail/CourseDetail'
+// import CourseDetail from './components/courseDetail/CourseDetail'
+import CourseResource from './components/courseResource/CourseResource'
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <CourseDetail courseKey={"LC0006"} />
+        {/* <CourseDetail courseKey={"LC0006"} /> */}
+        <CourseResource courseKey={"LC0006"} />
       </div>
     );
   }
