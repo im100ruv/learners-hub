0a036d6cd3ab9b6d40d83b16c3b696ce7be269ff
Akshay Kumar <pawan.akshaykr@gmail.com>
2018-06-22 13:01:43 +0530

    added header and dashboard structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/header/Header';
+import DashBoard from './components/dashboard/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        
+        <div><Header/></div>
+        <div><DashBoard/></div>
       </div>
     );
   }
---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import './Dashboard.css';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ProfileIcon from '@material-ui/icons/AccountCircle';
+import CoursesIcon from '@material-ui/icons/LibraryBooks';
+import SettingIcon from '@material-ui/icons/Settings';
+
+class Dashboard extends Component {
+
+  theme = {
+    root: {
+      flexGrow: 1,
+      height: 'calc(100vh - 85px)',
+      zIndex: 1,
+      overflow: 'hidden',
+      position: 'relative',
+      display: 'flex',
+    },
+    content: {
+      flexGrow: 1,
+      padding: '5px',
+      minWidth: 0,
+     height: 'calc(100vh - 85px)',
+     marginTop: '65px'
+    }
+  }
+
+  render() {
+    return (
+      <div className="dashboard">
+        <div style={this.theme.root}>
+          <AppBar position="absolute" className="dashborad-appbar">
+            <Toolbar>
+              <Typography variant="title" color="inherit" noWrap>
+                Dashboard
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            variant="permanent"
+            className="dashboard-drawer"
+          >
+            <List>
+              <ListItem button>
+                <ListItemIcon>
+                  <ProfileIcon />
+                </ListItemIcon>
+                <ListItemText primary="Profile" />
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <CoursesIcon />
+                </ListItemIcon>
+                <ListItemText primary="Courses Enrolled" />
+              </ListItem>
+            </List>
+            <Divider />
+            <List>
+            <ListItem button>
+                <ListItemIcon>
+                  <SettingIcon />
+                </ListItemIcon>
+                <ListItemText primary="Setting" />
+              </ListItem>
+            </List>
+          </Drawer>
+          <main style={this.theme.content}>
+            <Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>
+          </main>
+        </div>
+      </div>
+      
+    );
+  }
+}
+  
+export default Dashboard;
+  
