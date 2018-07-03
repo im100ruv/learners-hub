7d52b504785558d6cd550a97cca934f8876167eb
abhay.gupta <abhay.gupta@mountblue.io>
2018-07-03 10:40:50 +0530

    add websocket code in server.js file in server side.

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,17 @@ import Header from './components/header/Header';
 import DashBoard from './components/dashboard/Dashboard';
 import './config/firebaseConfig';
 // import { Provider } from 'react-redux';
-// import store from './store/store';
+// // import store from './store/store';
+// import socket from 'socket.io';
 
+// const socket =io.connect("http://localhost:8000");
+import SocketIOClient from 'socket.io-client';
 class App extends Component {
-
+constructor(){
+  super();
+this.socket = SocketIOClient('http://localhost:8000');
+}
+  
  render() {
 
    return (
